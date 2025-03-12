166fc28770cff05ace01bb7a3898e99bb17cdc2a
Omer <omeroztoprak003@gmail.com>
2025-03-12 23:25:54 +0300

    add database

--- api/config/index.js
+++ api/config/index.js
@@ -1,5 +1,5 @@
 module.exports = {
+    "PORT": process.env.PORT || "3000",
     "LOG_LEVEL": process.env.LOG_LEVEL || "debug",
-    "CONNECTİON_STRİNG": process.env.CONNECTION_STRING || "mangodb://27017/node_js_project"
-
+    "CONNECTION_STRING": process.env.CONNECTION_STRING || "mongodb://127.0.0.1:27017/project_base"
 }
---
+++ api/db/models/AuditLogs.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+
+const schema = mongoose.Schema({
+    level: String,
+    email: String,
+    password: String,
+    location: String,
+    proc_type: String,
+    log: String
+
+},{
+    timestamps: {
+        createdAt: "created_at",
+        uptadetAt: "updated_at"
+    }
+})
+
+class AuditLogs extends mongoose.Model {
+
+}
+
+schema.loadClass(AuditLogs);
+module.exports = mongoose.model("audit_logs", schema)
